8ef051d5123e4756b01bf89f2aeb0e43b8847a88
Ahmet Kaya <academyakm0@gmail.com>
2025-01-03 15:47:16 +0300

    Form App Create

---
+++ FormsApp/Models/Product.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FormsApp.Models{
+
+    public class Product{
+
+        [Display(Name = "Ürün Id")]
+        public int ProductId { get; set; }
+
+        [Display(Name = "Ürün Adı")]
+        [Required(ErrorMessage = "Ürün adı zorunlu!")]
+        [StringLength(100)]
+        public string? Name {get;set;} 
+
+        [Required]
+        [Range(0,100000)]
+        public decimal Price {get;set;}
+
+        [Required]
+        public string? Image {get;set;} = string.Empty;
+        public bool IsActive {get;set;}
+
+        [Required]
+        public int? CategoryId {get;set;}
+    }
+}
---
+++ FormsApp/Models/Repository.cs
@@ -0,0 +1,25 @@
+namespace FormsApp.Models{
+
+    public class Repository{
+
+        private static readonly List<Product> _products = new();
+        private static readonly List<Category> _categories = new();
+
+        static Repository(){
+            _categories.Add(new Category {CategoryId = 1, Name = "Telefon"});
+            _categories.Add(new Category {CategoryId = 2, Name = "Bilgisayar"});
+
+            _products.Add(new Product{ProductId= 1, Name = "Iphone 16", Price = 85000, IsActive = true, Image = "3.jpg", CategoryId = 1});
+            _products.Add(new Product{ProductId= 2, Name = "Iphone 16 Pro Max", Price = 115000, IsActive = true, Image = "4.jpg", CategoryId = 1});
+            _products.Add(new Product{ProductId= 3, Name = "Macbook Pro", Price = 105000, IsActive = true, Image = "1.jpg", CategoryId = 2});
+            _products.Add(new Product{ProductId= 4, Name = "Macbook Air", Price = 95000, IsActive = true, Image = "2.jpeg", CategoryId = 2});
+        }
+
+        public static List<Product> Products{get{return _products;}}
+        public static List<Category> Categories{get{return _categories;}}
+
+        public static void CraeteProduct(Product entity){
+            _products.Add(entity);
+        }
+    }
+}
